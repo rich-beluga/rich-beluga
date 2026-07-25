3975673b3d38eb149bf56cede8d010b38a08adb1
rich_beluga <znullv2@gmail.com>
2026-07-25 15:51:44 +0300

    fix: fix Heroku installation script

--- install-heroku-termux.sh
+++ install-heroku-termux.sh
@@ -6,22 +6,18 @@ echo -e ' \e[2m by \e[1mrich_beluga\e[0m'
 echo -e ' \e[1m My \e[38;2;131;176;255mTelegram:\e[0m \e[1m@rich_beluga'
 echo -e '  \e[2m---------\e[0m'
 
-environment() {
+install() {
     echo -e ' \e[1m Checking environment...\e[0m'
     cat /etc/os-release &> /dev/null || return 1
     echo -e '  \e[2m---------\e[0m'
-}
-
-check() {
+    
     echo -e ' \e[1m Checking packages...\e[0m'
     apt update > /dev/null || return 1
     apt install python3 python3-pip python3-venv git -y > /dev/null || return 1
     echo -e '  Packages installed \e[1;38;2;186;255;154msuccessfully\e[0m'
     which python3 > /dev/null || return 1
     echo -e '  \e[2m---------\e[0m'
-}
-
-install() {
+    
     echo -e ' \e[1m Installing dependencies...\e[0m'
     [ -d Heroku ] || git clone https://github.com/coddrago/Heroku &> /dev/null || return 1
     echo -e '  \e[1m Just wait...\e[0m'
@@ -32,16 +28,10 @@ install() {
     pip install -r requirements.txt > /dev/null || return 1
     echo -e '  Requirements installed \e[1;38;2;186;255;154msuccessfully\e[0m'
     echo -e '  \e[2m---------\e[0m'
-}
-
-start() {
+    
     echo -e ' \e[1m Heroku userbot installed, please run \e[33msource .venv/bin/activate ; cd ~/Heroku ; python3 -m heroku --no-web --root\e[0m'
     # '--no-web' - to avoid problems with code delivery
     # '--root'   - skip 'You attempted to run Heroku on behalf of root user. Please, create a new user and restart script' error
 }
 
-# calling all functions in order
-environment # check environment and install debian (proot-distro)
-check       # check and install packages
-install     # download Heroku
-start       # starting Heroku Userbot
+install     # install and start Heroku
